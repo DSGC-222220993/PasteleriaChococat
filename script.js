77e92e6500cc963f9bfdbd5f2815c4a582925f49
DSGC <a222220993@unison.mx>
2026-01-16 10:48:47 -0700

    Actualizacion

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Pasteleria Chococat cargada correctamente");
 })
+
+const socialItems=document.querySelectorAll(".social-item");
+socialItems.forEach(item => {
+    item.addEventListener("click", function(e) {
+        const link = this.getAttribute("href");
+        if (link){
+            console.log(`Redirigiendo a: ${link}`);
+            window.open(link, "_blank");
+        }   
+    });
+});
